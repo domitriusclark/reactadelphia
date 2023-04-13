78493462008a058e6ce12f122f1cb5db3562df56
Domitrius Clark <domitriusclark@Domitriuss-MBP.lan>
2023-04-12 20:02:47 -0400

    Added another method for our Discord client.

--- lib/discord.ts
+++ lib/discord.ts
@@ -250,3 +250,19 @@ export function getForum(channels: APIChannel[]): APIGuildForumChannel {
 
   return forum;
 }
+
+export async function getGuildEvents() {
+  const res = await fetch(
+    `https://discord.com/api/v9/guilds/${process.env.DISCORD_GUILD_ID}/scheduled-events`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      },
+    }
+  );
+
+  const data = await res.json();
+
+  return data;
+}
